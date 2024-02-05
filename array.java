cfb3b61b95e0e32c3a2ec3418e15ce442937bfd2
OmIngle007 <2022bit004@sggs.ac.in>
2024-02-05 22:54:37 +0530

    first commit

---
+++ array.java
@@ -0,0 +1,30 @@
+class arrayprint{
+	static void myfunction(){
+	   
+	   for(int i=0;i<3;i++){
+	        int myarray[]={1,2,3};
+		System.out.print(myarray[i] + " ");
+	   }
+        }
+        
+        static void first(){
+        int myarray[]={1,2,3};
+        System.out.print(myarray[0]);
+        }
+        
+         static void last(){
+        int myarray[]={1,2,3};
+        System.out.print(myarray[2]);
+        }
+        
+         static void secondlast(){
+        int myarray[]={1,2,3};
+        System.out.print(myarray[]);
+        }
+	
+	public static void main(String args[]){
+		
+		myfunction();
+		first();
+	}
+}
---
+++ reg.java
@@ -0,0 +1,18 @@
+class Reg{
+	
+	static void check(){
+		String[] array={"2022BIT001",  "2022BIT002","2022BIT003","2022BIT004","2022BIT005"};
+		for(int i=0;i<array.length;i++)
+		System.out.print(array[i]+" ");
+	}
+	
+	
+	
+	public static void main(String[] args){
+		check();
+		
+	}
+
+
+
+}
