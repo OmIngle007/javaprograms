4e7bd2aca0dcecf662d2cbe71e13fa34c460b895
OmIngle007 <2022bit004@sggs.ac.in>
2025-04-24 14:20:32 +0530

    final

--- Desktop/MERN_Project/Backend/src/server.js
+++ Desktop/MERN_Project/Backend/src/server.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error("MongoDB Connection error", err))
